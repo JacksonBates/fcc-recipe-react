32194f1bbf52b3a7969dc1d91d8361eac536db9e
jacksonbates <malgalin@gmail.com>
2017-10-17 20:29:17 +1100

    initial commit
    uses personal boilerplate to hello world the app, babel, webpack, react etc

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RecipeBookApp from './components/RecipeBookApp';
+// import 'normalize.css/normalize.css';
+// import './styles/styles.scss';
+
+ReactDOM.render(<RecipeBookApp />, document.getElementById('app'));

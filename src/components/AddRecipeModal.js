d4e86f1457b1842655c7687f1445241808a6ff01
jacksonbates <malgalin@gmail.com>
2017-11-23 21:54:52 +1100

    WIP - styling modals

--- src/components/AddRecipeModal.js
+++ src/components/AddRecipeModal.js
@@ -6,15 +6,18 @@ const AddRecipeModal = (props) => {
     <Modal
       isOpen={props.addModalOpen}
       onRequestClose={props.handleCloseAdd}
+      className='modal'
     >
-      <h3>Add a recipe</h3>
-      <form onSubmit={props.addRecipe}>
-        <label htmlFor='recipeInput'>Recipe</label><br />
-        <input name='recipeInput' type='text' placeholder='Recipe name' /><br />
-        <label htmlFor='ingredientsInput'>Ingredients</label><br />
-        <input name='ingredientsInput' type='text' placeholder='Ingredients should be comma separated' /><br />
-        <button>Add</button>
-      </form>
+      <h3 className='modal__title'>Add a recipe</h3>
+      <div className='modal__body'>
+        <form onSubmit={props.addRecipe}>
+          <label htmlFor='recipeInput'>Recipe</label><br />
+          <input name='recipeInput' type='text' placeholder='Recipe name' /><br />
+          <label htmlFor='ingredientsInput'>Ingredients</label><br />
+          <input name='ingredientsInput' type='text' placeholder='Ingredients should be comma separated' /><br />
+          <button>Add</button>
+        </form>
+      </div>
 
     </Modal>
   )
